9d1e2d54fb7f1226cde26d918bc874c801d1951e
Oliver Zernikow <zernikowoliver@gmail.com>
2019-07-19 18:46:00 +0200

    remove 2.0 features

--- lib/view_models/select_period_vm.dart
+++ lib/view_models/select_period_vm.dart
@@ -23,30 +23,3 @@ class SelectedPeriodViewModel {
     this.selectedDay,
   });
 }
-
-/* class PeriodMode {
-  SelectedPeriodMode _mode = SelectedPeriodMode.DATE;
-  SelectedPeriodMode get mode => _mode;
-
-  DateTime _date;
-  TimeOfDay _time;
-
-  DateTime get date => _date;
-  TimeOfDay get time => _time;
-
-  PeriodMode({DateTime date, TimeOfDay time, SelectedPeriodMode mode}) {
-    _date = date ?? DateTime.now().add(Duration(days: 1));
-    _time = time ?? TimeOfDay(hour: 8, minute: 0);
-    _mode = mode;
-  }
-
-  dynamic getSelectedValue() {
-    return _mode == SelectedPeriodMode.DATE ? _date : _time;
-  }
-
-}
-
-enum SelectedPeriodMode {
-  DATE,
-  TIME,
-} */
